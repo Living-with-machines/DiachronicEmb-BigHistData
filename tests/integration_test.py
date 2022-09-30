2df3389ded454e00dc8b7f7d02b35a043629f462
Nilo Pedrazzini <nilo.pedrazzini@gmail.com>
2022-09-30 12:51:56 +0100

    add tests

---
+++ tests/integration_test.py
@@ -0,0 +1,226 @@
+# Test texts in test_data/ generated randomly from https://randomtextgenerator.com
+
+import pytest
+
+from glob import glob
+import yaml
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize.regexp import regexp_tokenize
+from gensim.models import Word2Vec
+from tqdm import tqdm
+import timeit
+import re
+
+import pytest
+import yaml
+
+from utils import smart_procrustes_align_gensim
+
+@pytest.fixture
+def test_configs():
+    with open("tests/test_config.yaml", "r") as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+@pytest.fixture
+def test_validation_function1(test_configs):
+    return {
+        'namethetest': test_configs['namethetest'],
+        'inputfiles': test_configs['inputs']['directory'],
+        'skip_preprocessing': test_configs['preprocessing']['skip'],
+        'skip_training': test_configs['training']['skip'],
+        'skip_alignment': test_configs['alignment']['skip'],
+
+        ## -- Preprocessing
+        'savepreprocessed': test_configs['preprocessing']['savepreprocessed'],
+        'minwordlength': test_configs['preprocessing']['pipelines']['minwordlength'],
+        'lowercase': test_configs['preprocessing']['pipelines']['lowercase'],
+        'remove_punctuation': test_configs['preprocessing']['pipelines']['remove_punctuation'],
+        'remove_stopwords': test_configs['preprocessing']['pipelines']['remove_stopwords'],
+
+    ## -- Training
+        'epochs': test_configs['training']['options']['epochs'],
+        'vector_size': test_configs['training']['options']['vector_size'],
+        'sg': test_configs['training']['options']['sg'],
+        'min_count': test_configs['training']['options']['min_count'],
+        'window': test_configs['training']['options']['window'],
+        'start_alpha': test_configs['training']['options']['start_alpha'],
+        'end_alpha': test_configs['training']['options']['end_alpha'],
+        'workers': test_configs['training']['options']['workers']
+        }
+
+
+# ------------------- Start timing the whole process --------------------
+def test_preprocess(test_validation_function1):
+    startall = timeit.default_timer()
+
+    # ------------------- Import configs --------------------
+    # --- Relevant vars
+    ## -- General
+    namethetest = test_validation_function1['namethetest']
+    inputfiles = test_validation_function1['inputfiles']
+    skip_preprocessing = test_validation_function1['skip_preprocessing']
+    skip_training = test_validation_function1['skip_training']
+    skip_alignment = test_validation_function1['skip_alignment']
+
+    ## -- Preprocessing
+    if skip_preprocessing == False:
+        savepreprocessed = test_validation_function1['savepreprocessed']
+        minwordlength = test_validation_function1['minwordlength']
+        lowercase = test_validation_function1['lowercase']
+        remove_punctuation = test_validation_function1['remove_punctuation']
+        remove_stopwords = test_validation_function1['remove_stopwords']
+
+    ## -- Training
+    if skip_training == False:
+        epochs = test_validation_function1['epochs']
+        vector_size = test_validation_function1['vector_size']
+        sg = test_validation_function1['sg']
+        min_count = test_validation_function1['min_count']
+        window = test_validation_function1['window']
+        start_alpha = test_validation_function1['start_alpha']
+        end_alpha = test_validation_function1['end_alpha']
+        workers = test_validation_function1['workers']
+
+    # ------------------- Create dirs if needed --------------------
+
+    @pytest.fixture(scope="session")
+    def needed_dirs(tmp_path_factory):
+    # --- General outputs directory
+        outdir = tmp_path_factory.mktemp("test_outputs")
+        outdir_namethetest = tmp_path_factory.mktemp(f"test_outputs/{namethetest}")
+        rawdir = tmp_path_factory.mktemp(f"test_outputs/{namethetest}/raw")
+        aligneddir = tmp_path_factory.mktemp(f"test_outputs/{namethetest}/aligned")
+        preprocesseddir = tmp_path_factory.mktemp(f"test_outputs/{namethetest}/preprocessed_corpus")
+        return {
+            'outdir': outdir,
+            'outdir_namethetest': outdir_namethetest,
+            'rawdir': rawdir,
+            'aligneddir': aligneddir,
+            'preprocesseddir': preprocesseddir
+        }
+
+    # ------------------- Preprocessing & training --------------------
+
+    # --- List the paths for all the texts inside the input dir
+    alltexts = glob(f'{inputfiles}/*txt')
+
+    # --- Define stopwords
+    cachedStopWords = stopwords.words("english")
+
+    ## -- To expand the default NLTK stopwords for English, uncomment the following three lines and add your own:
+    #newstopwords = ['mrs','mr','say','says','said','tell','told','seem','seems','seemed','ask','asks','asked','upon','aboard','about','above','account','across','after','against','ago','ahead','along','alongside','amid','among','around','aside','at','atop','away','because','before','behalf','behind','below','beneath','beside','besides','between','beyond','but','by','circa','considering','depending','despite','down','due','during','for','from','further','given','in','including','inside','instead','into','less','like','near','notwithstanding','of','off','on','onto','opposite','other','out','outside','over','owing','per','plus','regarding','regardless','round','since','than','thanks','through','throughout','till','to','together','toward','towards','under','underneath','unlike','until','up','upon','versus','via','with','within','without']
+    #cachedStopWords.extend(newstopwords)
+    #cachedStopWords = list(dict.fromkeys(cachedStopWords))
+
+    for subcorpus in alltexts:
+        # --- Start timing for the specific time slice
+        startslice = timeit.default_timer()
+
+        # --- Get name of the time slice (i.e. the last bit of the file path without the ext)
+        nameoftimeslice = subcorpus.split('/')[-1].split('.')[0]
+
+        print(f'Now preprocessing {nameoftimeslice}...')
+
+        # --- Open new file in w mode if option to save the preprocessed text is chosen
+        
+        if savepreprocessed == True:
+            newfile = open(f"{needed_dirs['preprocesseddir']}/{nameoftimeslice}.txt", 'w')
+        
+        # --- Start list of sentences (i.e. articles, text chunks, etc., depending on input), this will be input of w2v
+        sentences = []
+
+# ------------------- stopwdsrm --------------------
+
+        with open(subcorpus) as infile:
+            # --- Read line by line and preprocess
+            for line in tqdm(infile):
+                ## -- Preprocess the texts according to configs
+                text = re.sub('-\n', '', line) # Remove OCR'd linebreaks within words if they exist
+                text = re.sub('\n', ' ', text) # Remove ordinary linebreaks (there shouldn't be, so this might be redundant)
+                if remove_punctuation == True:
+                    text = re.sub(r'[^a-zA-Z\s]', ' ', str(text)) # Remove anything that is not a space, a letter, or a number
+                if lowercase == True:
+                    text = str(text).lower()
+                ## -- Remove stopwords if chosen to
+                if remove_stopwords == True:
+                    text = ' '.join([word for word in text.split() if word not in cachedStopWords and len(word) >= minwordlength])
+                ## -- Write the line to the newfile if chosen to (without tokenizing)
+                if savepreprocessed == True:
+                    if text != ' ' and text != '' and text != '\n':
+                        newfile.write(text + '\n')
+                ## -- Tokenize if what's left after preprocessing is not just whitespace
+                if skip_training == False:
+                    if text != ' ' and text != '' and text != '\n':
+                        text = regexp_tokenize(text, pattern='\s+', gaps =True)
+                        sentences.append(text)
+        
+        print('All preprocessing finished. Altogether it took {} minutes.'.format((timeit.default_timer() - startslice) / 60))
+        
+        if skip_training == False:
+            # --- Initialize w2v model with selected parameters
+            model = Word2Vec(min_count=min_count,
+                                window=window, 
+                                sg=sg,
+                                vector_size=vector_size,
+                                workers=workers)
+
+            # --- Build vocabulary from the sentences based on the initialized model
+            print('Building the vocab...')
+            model.build_vocab(sentences)
+            print('Vocab built!')
+            
+            # --- Start the training
+            print('Now training the {}s model... This might take a very long time. Have a coffee. Or two.'.format(nameoftimeslice))
+
+            # --- Start timing for training process
+            traintime = timeit.default_timer()
+            
+            model.train(sentences, 
+                            total_examples=model.corpus_count, 
+                            start_alpha=start_alpha, 
+                            end_alpha=end_alpha, 
+                            epochs=epochs)
+            
+            # --- Save model
+            model.save(f"{needed_dirs['rawdir']}/{nameoftimeslice}.model")
+
+            stopslice = timeit.default_timer()
+            print('{} done! It took {} minutes'.format(nameoftimeslice, (stopslice - startslice)/60))
+
+    # ------------------- Alignment --------------------
+
+        if skip_alignment == False:
+            print('Now starting alignement with Orthogonal Procrustes...')
+            # --- Start timing alignment process
+            startalign = timeit.default_timer()
+
+            # --- List all raw models and order them by name
+            allmodels = sorted(glob(f"{needed_dirs['rawdir']}/*model"))
+
+            # --- Load last model (fixed time slice)
+            model1 = Word2Vec.load(allmodels[-1])
+
+            # --- Just get the model name and make a copy in the aligned folder
+            model1_name = allmodels[-1].split('/')[-1]
+            model1.save(f"{needed_dirs['aligneddir']}/{model1_name}")
+
+            # --- Aligned each of the models to the model for the last time slice
+            for model in allmodels[:-2]:
+
+                model2_name = model.split('/')[-1]
+                print('Now aligning {} to {}...'.format(model1_name,model2_name))
+                
+                ## -- Load model
+                model2 = Word2Vec.load(model)
+
+                ## -- Temp create a third model and save it in the aligned folder
+                model3 = smart_procrustes_align_gensim(model1,model2)
+
+                model3.save(f"{needed_dirs['aligneddir']}/{model2_name}")
+
+            print('Alignment complete!')
+            print('Time to finish alignment: {} mins'.format(round((timeit.default_timer() - startalign) / 60, 2)))
+
+        print('Time to finish the whole process: {} mins'.format(round((timeit.default_timer() - startall) / 60, 2)))
---
+++ tests/utils.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+# ------------------- smart_procrustes_align_gensim --------------------
+# from: https://gist.github.com/quadrismegistus/09a93e219a6ffc4f216fb85235535faf
+def smart_procrustes_align_gensim(base_embed, other_embed, words=None):
+    """
+    Original script: https://gist.github.com/quadrismegistus/09a93e219a6ffc4f216fb85235535faf
+    Procrustes align two gensim word2vec models (to allow for comparison between same word across models).
+    Code ported from HistWords <https://github.com/williamleif/histwords> by William Hamilton <wleif@stanford.edu>.
+        
+    First, intersect the vocabularies (see `intersection_align_gensim` documentation).
+    Then do the alignment on the other_embed model.
+    Replace the other_embed model's syn0 and syn0norm numpy matrices with the aligned version.
+    Return other_embed.
+
+    If `words` is set, intersect the two models' vocabulary with the vocabulary in words (see `intersection_align_gensim` documentation).
+    """
+
+    # patch by Richard So [https://twitter.com/richardjeanso) (thanks!) to update this code for new version of gensim
+    # base_embed.init_sims(replace=True)
+    # other_embed.init_sims(replace=True)
+
+    # make sure vocabulary and indices are aligned
+    in_base_embed, in_other_embed = intersection_align_gensim(base_embed, other_embed, words=words)
+
+    # re-filling the normed vectors
+    in_base_embed.wv.fill_norms(force=True)
+    in_other_embed.wv.fill_norms(force=True)
+
+    # get the (normalized) embedding matrices
+    base_vecs = in_base_embed.wv.get_normed_vectors()
+    other_vecs = in_other_embed.wv.get_normed_vectors()
+
+    # just a matrix dot product with numpy
+    m = other_vecs.T.dot(base_vecs) 
+    # SVD method from numpy
+    u, _, v = np.linalg.svd(m)
+    # another matrix operation
+    ortho = u.dot(v) 
+    # Replace original array with modified one, i.e. multiplying the embedding matrix by "ortho"
+    other_embed.wv.vectors = (other_embed.wv.vectors).dot(ortho)    
+    
+    return other_embed
+
+# ------------------- intersection_align_gensim --------------------
+# from: https://gist.github.com/quadrismegistus/09a93e219a6ffc4f216fb85235535faf
+def intersection_align_gensim(m1, m2, words=None):
+    """
+    Intersect two gensim word2vec models, m1 and m2.
+    Only the shared vocabulary between them is kept.
+    If 'words' is set (as list or set), then the vocabulary is intersected with this list as well.
+    Indices are re-organized from 0..N in order of descending frequency (=sum of counts from both m1 and m2).
+    These indices correspond to the new syn0 and syn0norm objects in both gensim models:
+        -- so that Row 0 of m1.syn0 will be for the same word as Row 0 of m2.syn0
+        -- you can find the index of any word on the .index2word list: model.index2word.index(word) => 2
+    The .vocab dictionary is also updated for each model, preserving the count but updating the index.
+    """
+
+    # Get the vocab for each model
+    vocab_m1 = set(m1.wv.index_to_key)
+    vocab_m2 = set(m2.wv.index_to_key)
+
+    # Find the common vocabulary
+    common_vocab = vocab_m1 & vocab_m2
+    if words: common_vocab &= set(words)
+
+    # If no alignment necessary because vocab is identical...
+    if not vocab_m1 - common_vocab and not vocab_m2 - common_vocab:
+        return (m1,m2)
+
+    # Otherwise sort by frequency (summed for both)
+    common_vocab = list(common_vocab)
+    common_vocab.sort(key=lambda w: m1.wv.get_vecattr(w, "count") + m2.wv.get_vecattr(w, "count"), reverse=True)
+    # print(len(common_vocab))
+
+    # Then for each model...
+    for m in [m1, m2]:
+        # Replace old syn0norm array with new one (with common vocab)
+        indices = [m.wv.key_to_index[w] for w in common_vocab]
+        old_arr = m.wv.vectors
+        new_arr = np.array([old_arr[index] for index in indices])
+        m.wv.vectors = new_arr
+
+        # Replace old vocab dictionary with new one (with common vocab)
+        # and old index2word with new one
+        new_key_to_index = {}
+        new_index_to_key = []
+        for new_index, key in enumerate(common_vocab):
+            new_key_to_index[key] = new_index
+            new_index_to_key.append(key)
+        m.wv.key_to_index = new_key_to_index
+        m.wv.index_to_key = new_index_to_key
+        
+        print(len(m.wv.key_to_index), len(m.wv.vectors))
+        
+    return (m1,m2)
